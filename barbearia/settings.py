9d25d29e62ef3f513a1b268b9dbf2775863b0227
ThiagoSilva1995 <106204148+ThiagoSilva1995@users.noreply.github.com>
2024-10-25 23:07:05 -0300

    quinto comit

--- barbearia/settings.py
+++ barbearia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k(5^003*4z1yu@1b4+eokmn1etn6fnoah$19u*nguspsj*7c9f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,8 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
-    
+
+
 ]
 
 ROOT_URLCONF = 'barbearia.urls'
@@ -135,6 +135,6 @@ MESSAGE_TAGS={
     messages.SUCCESS:'success',
 }
 
-
-LOGIN_URL = 'login'  # URL para a página de login
 LOGIN_REDIRECT_URL = 'admin_custom'
+
+
